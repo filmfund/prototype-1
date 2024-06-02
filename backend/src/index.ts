9a1e0074f49e1b8f592b4cb718ee3c78b62e7f7f
Prokop <prokop.schield@protonmail.com>
2024-06-02 10:26:09 +0200

    fix(backend): fix bigint serialization

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,15 @@
+import { defineGlobalProperty } from "ps-std";
+
 import { loadAll } from "./modules/loader";
 
 export async function main() {
+	defineGlobalProperty("toJSON", function toJSON(this: any) {
+		try {
+			return String(this);
+		} catch {
+			return "<SerializationError>";
+		}
+	});
+
 	await loadAll();
 }
