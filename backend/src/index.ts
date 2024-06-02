baea3e3fa46862fa64180bf7159a62033d3d33ab
Prokop <prokop.schield@protonmail.com>
2024-06-02 10:28:48 +0200

    fix(backend): apply toJSON only to BigInt.prototype

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,17 @@ import { defineGlobalProperty } from "ps-std";
 import { loadAll } from "./modules/loader";
 
 export async function main() {
-	defineGlobalProperty("toJSON", function toJSON(this: any) {
-		try {
-			return String(this);
-		} catch {
-			return "<SerializationError>";
+	Object.defineProperty(
+		BigInt.prototype,
+		"toJSON",
+		function toJSON(this: any) {
+			try {
+				return String(this);
+			} catch {
+				return "<SerializationError>";
+			}
 		}
-	});
+	);
 
 	await loadAll();
 }
